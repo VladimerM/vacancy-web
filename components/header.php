bdc41a3e3d85508cd6d6a09d40eb6b8092bc8c56
VladimerM <76262102+VladimerM@users.noreply.github.com>
2025-06-09 21:13:28 +0400

    update header

--- components/header.php
+++ components/header.php
@@ -1,9 +1,11 @@
 <?php
+$basePath = '/job-portal';
+
 $navItems = [
-    'Home' => '/job-portal/index.php',
-    'Jobs' => '/job-portal/pages/jobs.php',
-    'About Us' => '/job-portal/pages/about.php',
-    'Contact Us' => '/job-portal/pages/contact.php'
+    'Home' => $basePath . '/index.php',
+    'Jobs' => $basePath . '/pages/jobs.php',
+    'About Us' => $basePath . '/pages/about.php',
+    'Contact Us' => $basePath . '/pages/contact.php'
 ];
 
 $currentPage = basename($_SERVER['PHP_SELF']);
@@ -11,8 +13,8 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 
 <header class="header">
     <div class="header__container">
-        <a href="/job-portal/index.php" class="header__logo">
-            <img src="/job-portal/assets/images/icons/logo.png" alt="Job Portal Logo" class="header__logo-icon">
+        <a href="<?php echo $basePath; ?>/index.php" class="header__logo">
+            <img src="<?php echo $basePath; ?>/assets/images/icons/logo.png" alt="Job Portal Logo" class="header__logo-icon">
             Job Portal
         </a>
 
@@ -21,7 +23,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 <?php foreach ($navItems as $name => $url): ?>
                     <li class="header__nav-item">
                         <a href="<?php echo $url; ?>"
-                            class="header__nav-link <?php echo ($currentPage === $url) ? 'header__nav-link--active' : ''; ?>">
+                            class="header__nav-link <?php echo ($currentPage === basename($url)) ? 'header__nav-link--active' : ''; ?>">
                             <?php echo $name; ?>
                         </a>
                     </li>
@@ -30,8 +32,8 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         </nav>
 
         <div class="header__actions">
-            <a href="login.php" class="header__login">Login</a>
-            <a href="register.php" class="header__register">Register</a>
+            <a href="<?php echo $basePath; ?>/login.php" class="header__login">Login</a>
+            <a href="<?php echo $basePath; ?>/register.php" class="header__register">Register</a>
         </div>
 
         <div class="header__burger">
@@ -47,7 +49,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 <?php foreach ($navItems as $name => $url): ?>
                     <li class="header__mobile-nav-item">
                         <a href="<?php echo $url; ?>"
-                            class="header__mobile-nav-link <?php echo ($currentPage === $url) ? 'header__mobile-nav-link--active' : ''; ?>">
+                            class="header__mobile-nav-link <?php echo ($currentPage === basename($url)) ? 'header__mobile-nav-link--active' : ''; ?>">
                             <?php echo $name; ?>
                         </a>
                     </li>
@@ -56,13 +58,13 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         </nav>
 
         <div class="header__mobile-actions">
-            <a href="login.php" class="header__mobile-login">Login</a>
-            <a href="register.php" class="header__mobile-register">Register</a>
+            <a href="<?php echo $basePath; ?>/login.php" class="header__mobile-login">Login</a>
+            <a href="<?php echo $basePath; ?>/register.php" class="header__mobile-register">Register</a>
         </div>
     </div>
 </header>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function () {
         function toggleMobileMenu() {
