ab80adf36365920b5f1d3c254046ccdfea86ec46
VladimerM <76262102+VladimerM@users.noreply.github.com>
2025-06-09 20:09:57 +0400

    add jobs php page

---
+++ pages/jobs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Web App</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+  <?php include '../components/header.php'; ?>
+  <h1>Hello World</h1>
+  <?php include '../components/footer.php'; ?>
+</body>
+
+</html>
