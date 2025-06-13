4fdbc40cf6da4e3a4908a65d119c19ec0c22ad8d
MarinaKhchoian <154251511+MarinaKhchoian@users.noreply.github.com>
2025-06-13 15:57:07 +0400

    /

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ class Database
       $this->conn = new PDO(
         "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
         $this->username,
-        $this->password
+        $this->password 
       );
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $exception) {
